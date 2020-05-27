9610b7e9ff796c96cf70c415b7dfa2a3100247f6
radhashree55 <63909545+radhashree55@users.noreply.github.com>
2020-05-27 18:11:16 +0530

    Add files via upload

---
+++ shoot4.js
@@ -0,0 +1,20 @@
+class Aim4 {
+    constructor(x,y,width,height) {
+      var options = {
+        'restitution':0,
+        'friction':1.0,
+        'density':1.0         
+      }
+      this.body = Bodies.rectangle(x,y,width,height,options);
+      this.width = width;
+      this.height = height;
+      World.add(world, this.body);
+    }
+    display(){
+      var pos =this.body.position;
+      rectMode(CENTER);
+      fill(225,0,255);
+      stroke(0);
+      rect(pos.x, pos.y, this.width, this.height);
+    }
+  };
